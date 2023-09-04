4b9ef043a571e68a35ab3af2a531f7d225fe9be5
Thiago Martins <thiagomartinsdsantos@gmail.com>
2023-09-04 07:20:06 -0300

    primeira publicação
    primeira publicação

---
+++ app-todo/src/app/modules/home/components/todo-button-delete-all/todo-button-delete-all.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-button-delete-all',
+  templateUrl: './todo-button-delete-all.component.html',
+  styleUrls: ['./todo-button-delete-all.component.scss']
+})
+export class TodoButtonDeleteAllComponent {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
---
+++ app-todo/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+//interface
+import { TaskList } from '../../model/task-list';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit{
+
+  public taskList: Array<TaskList> = [
+    { task: "Minha nova Task", checked: true},
+    { task: "Minha nova Task2", checked: false},
+  ];
+
+  constructor(){ }
+  ngOnInit(): void {
+      
+  }
+
+  public deleteItemTaskList(event: number){
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList(){
+    this.taskList = [];
+  }
+}
